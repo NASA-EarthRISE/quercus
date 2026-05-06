d64cb666e77965ed29cbf3bcd317ac56c45f667d
Tim Mayer <45499910+MayerT1@users.noreply.github.com>
2026-05-06 16:26:21 -0500

    Update __init__.py

--- quercus/ingest/__init__.py
+++ quercus/ingest/__init__.py
@@ -1,1 +1,4 @@
- 
+from quercus.ingest.download import fetch_iiif_images
+from quercus.ingest.preprocess import clip_scan_borders
+from quercus.ingest.georeference import georeference_images
+from quercus.ingest.mosaic import build_mosaic
